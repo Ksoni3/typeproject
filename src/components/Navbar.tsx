ed46fa992d3c3edf36d4a53ebb479c16ae44d25f
Kamal Soni <iamkamalsoni7@gmail.com>
2023-04-23 18:59:58 +1000

    added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/navbar.css";
 
 const Navbar: React.FC = () => {
   return (
-    <div>
-      <ul>
+    <div className="container">
+      <ul className="wrapper">
         <li>
-          <Link to={"/data"}>Home</Link>
+          <Link className="links" to={"/data"}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to={"/data"}>Contact</Link>
+          <Link className="links" to={"/data"}>
+            Contact
+          </Link>
         </li>
         <li>
-          <Link to={"/data"}>About</Link>
+          <Link className="links" to={"/data"}>
+            About
+          </Link>
         </li>
       </ul>
     </div>
